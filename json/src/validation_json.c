57b2653fc3d4eb1515eca625115e561177516eea
cherifyounouss <chinsanity16@gmail.com>
2018-12-28 05:14:47 +0000

    source validation json

---
+++ json/src/validation_json.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "../include/JSON_checker.h"
+
+int est_valide(FILE* fic){
+    char ch;
+    JSON_checker jc = new_JSON_checker(20);
+
+    while(1){
+        ch = fgetc(fic);
+        /* sortie lorsqu'on atteint la fin du fichier*/
+        if(feof(fic))
+            break;
+        /* Verification syntaxique */
+        if (!JSON_checker_char(jc, ch)) {
+            fprintf(stderr, "JSON_checker_char: erreur de syntaxe %c\n", ch);
+            return 0;
+        }
+    }
+    return 1;
+}
