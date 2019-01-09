17f2799ab85478e872df9913cfad070e7465295b
cheikhmbacke <serignecheikhmbackethiaw@gmail.com>
2019-01-09 15:28:01 +0000

    Modification de la validation et extraction des données

--- xml/src/extractData.c
+++ xml/src/extractData.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
-#include "../include/libxml/tree1.c"
+// #include <libxml/tree1.h>
+#include "../include/libxml/tree1.h"
 
 int main(int argc, char **argv)
 {
@@ -12,22 +13,14 @@ int main(int argc, char **argv)
         fprintf(stderr,"Veuiller renseigner le fichier xml\n");
         return(1);
     }
-
-    /*
-     * this initialize the library and check potential ABI mismatches
-     * between the version it was compiled for and the actual shared
-     * library used.
-     */
-    LIBXML_TEST_VERSION
-
+    
     /*parse the file and get the DOM */
     doc = xmlReadFile(argv[1], NULL, 0);
 
     if (doc == NULL) {
         printf("error: could not parse file %s\n", argv[1]);
-    }
-
-    /*Get the root element node */
+    }else{
+        /*Get the root element node */
     root_element = xmlDocGetRootElement(doc);
 
     print_element_names(root_element);
@@ -40,6 +33,7 @@ int main(int argc, char **argv)
      *have been allocated by the parser.
      */
     xmlCleanupParser();
+    }
 
     return 0;
 }
--- xml/src/xmlValidFile.c
+++ xml/src/xmlValidFile.c
@@ -1,5 +1,48 @@
 #include <stdio.h>
-#include "../include/libxml/reader2.c"
+#include <libxml/reader2.h>
+
+/**
+ * streamFile:
+ * @filename: the file name to parse
+ *
+ * Parse, validate and print information about an XML file.
+ */
+static void
+streamFile(const char *filename) {
+    xmlTextReaderPtr reader;
+    int ret;
+
+
+    /*
+     * Pass some special parsing options to activate DTD attribute defaulting,
+     * entities substitution and DTD validation
+     */
+    reader = xmlReaderForFile(filename, NULL,
+         XML_PARSE_DTDATTR |  /* default DTD attributes */
+		 XML_PARSE_NOENT |    /* substitute entities */
+		 XML_PARSE_DTDVALID); /* validate with the DTD */
+    if (reader != NULL) {
+        ret = xmlTextReaderRead(reader);
+        // while (ret == 1) {
+        //     processNode(reader);
+        //     ret = xmlTextReaderRead(reader);
+        // }
+	/*
+	 * Once the document has been fully parsed check the validation results
+	 */
+        if (xmlTextReaderIsValid(reader) != 1) {
+            fprintf(stderr, "Le document %s n'est pas valide\n", filename);
+        }else{
+            fprintf(stderr, "Le document %s est valide\n", filename);
+        }
+            xmlFreeTextReader(reader);
+            if (ret != 0) {
+                fprintf(stderr, "Erreur lors de la lecture du fichier %s\n", filename);
+            }
+    } else {
+        fprintf(stderr, "Erreur d'ouverture %s\n", filename);
+    }
+}
 
 int main(int argc, char **argv) {
     if (argc != 2){
