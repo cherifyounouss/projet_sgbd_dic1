1ec56103e1c0c8093be2e6b54e984148cea4d351
cherifyounouss <chinsanity16@gmail.com>
2019-01-10 21:44:53 +0000

    code pour validation json

--- json/src/validation_json.c
+++ json/src/validation_json.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "../include/JSON_checker.h"
 
-int est_valide(FILE* fic){
+int est_valide(char* filename){
     char ch;
+    FILE* fic = NULL;
+    fic = fopen(filename, "r");
+    if (!fic) {
+        return EXIT_FAILURE;
+    }
+    
     JSON_checker jc = new_JSON_checker(20);
 
     while(1){
@@ -12,13 +19,13 @@ int est_valide(FILE* fic){
             break;
         /* Verification syntaxique */
         if (!JSON_checker_char(jc, ch)) {
-            fprintf(stderr, "JSON_checker_char: erreur de syntaxe %c\n", ch);
-            return 0;
+            fprintf(stderr, "JSON_checker_char: erreur de syntaxe dans le fichier json %c\n", ch);
+            exit(1);
         }
     }
     if (!JSON_checker_done(jc)) {
         fprintf(stderr, "JSON_checker_end: erreur de syntaxe dans le fichier json\n");
         exit(1);
     }
-    return 1;
+    return 0;
 }
