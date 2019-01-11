5a80f2ac8c83a61ffde829697bd24f2193dd42ca
cheikhmbacke <serignecheikhmbackethiaw@gmail.com>
2019-01-11 07:27:36 +0000

    Mise à jour du fichier d'extraction

--- xml/src/extractData.c
+++ xml/src/extractData.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <libxml/parser.h>
-#include <libxml/tree.h>
-#include "saveFile.c"
+#include <libxml/tree.h>    
+#include "../include/extractData.h"
 
 /**
  * printData:
@@ -18,13 +18,15 @@ FILE *f;
 /*
 * entite est un tableau contenant l'ensemble des entités extraites
 */
-s_entite *entite;
+// s_entite *entite;
 /*
 * assoc est un tableau contenant l'ensemble des associations extraites
 */
-s_assoc *assoc;
+// s_assoc *assoc;
+// nb_entites n_entite;
+// nb_assos n;
 
-static void printData(xmlNode * a_node)
+void printData(xmlNode * a_node)
 {
     xmlNode *cur_node = NULL;
     /*
@@ -38,54 +40,29 @@ static void printData(xmlNode * a_node)
         if (cur_node->type == XML_ELEMENT_NODE) {
             save_data(cur_node->name,f);
             // strcpy(entite->nom,cur_node->name);
-                printf("Element: %s\n",cur_node->name);
+                // printf("Element: %s\n",cur_node->name);
+                // if(strcmp(cur_node->name,"entite") == 0){
+                //     strcpy(entite->nom,cur_node->name);
+                //     entite->nb_assoc++;
+                //     n_entite++;
+                // }
+                // if(strcmp(cur_node->name,"association") == 0){
+                //     strcpy(assoc->nom,cur_node->name);
+                //     n++;
+                // }
             }
         /*
         * On recupére la valeur de l'élément
         */
         if (cur_node->type == XML_TEXT_NODE) {
                 save_data(cur_node->content,f);
-                printf("valeur: %s\n",cur_node->content);      
+                // printf("valeur: %s\n",cur_node->content);      
         }
             /*
             * Appel récursif
             */
             printData(cur_node->children);
+            // generer_nb_entites_association(n_entite,n);
     }
 }
 
-    /*
-     * Un bloc main pour tester la fonction d'extraction
-     */
-int main(int argc, char **argv)
-{
-    xmlDoc *doc = NULL;
-    xmlNode *root_element = NULL;
-
-    if (argc != 2){
-        fprintf(stderr,"Veuiller renseigner le fichier xml\n");
-        return(1);
-    }
-    
-    /*Analyse du fichier et recuperation de l'ensemble des noeuds */
-    doc = xmlReadFile(argv[1], NULL, 0);
-
-    if (doc == NULL) {
-        printf("Erreur survenue lors de l'ouverture du fichier %s\n", argv[1]);
-    }else{
-        /*Recuperation du noeud racine*/
-    root_element = xmlDocGetRootElement(doc);
-
-    printData(root_element);
-
-    /*On libére la variable doc */
-    xmlFreeDoc(doc);
-
-    /*
-     * On libére les ressources libxml2 utilisées par la fonction.
-     */
-    xmlCleanupParser();
-    }
-
-    return 0;
-}
