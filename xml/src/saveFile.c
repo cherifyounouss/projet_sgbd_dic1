11b68aa1d2788c009b8d03904d0d3710f99dec63
cheikhmbacke <serignecheikhmbackethiaw@gmail.com>
2019-01-11 07:28:10 +0000

    Fichier de sauvegarde de donées

--- xml/src/saveFile.c
+++ xml/src/saveFile.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
 #include <libxml/xmlstring.h>
+#include <libxml/tree.h>
+#include "../include/extractData.h"
 
-static void save_data(const xmlChar *data,FILE *fic){
+void save_data(const xmlChar *data,FILE *fic){
 
-    fic=fopen("saveFile.txt","a");
+    fic=fopen("saveFile.txt","a+");
 
-    if (strcmp(data,"") == 0){
-        exit(1);
-    }
+    // if (strcmp(data,"") == 0){
+    //     exit(1);
+    // }
     // fseek(fic,5,0);
     fprintf(fic,"%s\n",data);
     // rewind(fic);
     fclose(fic);
 }
-
-typedef struct s_entite{
-    /* data */
-    char *nom;
-    char **attribut;
-    char **associations;
-    int nb_assoc;
-    int nb_attributs;
-
-}s_entite;
-
-typedef struct s_assoc{
-    /* data */
-    char *nom;
-    char **attribut;
-    int nb_attributs;
-
-}s_assoc;
 
 
