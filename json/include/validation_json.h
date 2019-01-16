1443433592511555ad83fac1d568536aabf9e5b5
cherifyounouss <chinsanity16@gmail.com>
2019-01-16 22:09:40 +0000

    header validation json updated

--- json/include/validation_json.h
+++ json/include/validation_json.h
@@ -2,3 +2,8 @@
 *   Valeur de retour: 0 si la validation reussi
 */
 int est_valide(char* filename);
+
+/* Cette fonction permet de creer un fichier dont le nom est fichier_flux et d'y stocker le resultat
+* de la requete http
+*/
+void flux_vers_fichier(char* flux_http);
