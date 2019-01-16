8381d532d0aff473951fc60ca63b038ae5f9efd5
cherifyounouss <chinsanity16@gmail.com>
2019-01-16 22:09:24 +0000

    header generation svg updated

--- svg/generation_svg.h
+++ svg/generation_svg.h
@@ -2,4 +2,4 @@ void dessiner_entite(cairo_t* cr, double x, double y, double width, double heigh
 double percentage(double x, double per_cent);
 void dessiner_association(cairo_t* cr, double xc, double yc, double radius, char* nom);
 int chercher_chaine(char** attributs, char* chaine, int n_attributs);
-void generer_svg(char* json_file, char* svg_file);
+void generer_svg(char* json_file, char* svg_file, int count_t);
