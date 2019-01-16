da6c7177b4dc0433845c5191121bafcc49a68399
cherifyounouss <chinsanity16@gmail.com>
2019-01-16 22:07:44 +0000

    header flux http

---
+++ flux_http.h
@@ -0,0 +1,9 @@
+int newline_offset(const char *text);
+size_t write_response(void *ptr, size_t size, size_t nmemb, void *stream);
+char *request(const char *url);
+
+struct write_result
+{
+    char *data;
+    int pos;
+};
